13c75660ce43d797d1d3aae144e609b4405a1bed
Zongyi <492605791@qq.com>
2026-05-08 17:48:16 +0800

    Initial commit

---
+++ StudentScoreManager/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentScoreManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Student
+    {
+        /// <summary>
+        /// 学号
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        public double Chinese { get; set; }
+        public double English { get; set; }
+        public double Math { get; set; }
+    }
+}
